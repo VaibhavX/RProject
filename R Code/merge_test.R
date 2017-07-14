68b546be9085d9122f698ec3bcaed4c59ebbf0b6
Vaibhav Vishal <vvishal@usc.edu>
2017-07-14 08:58:30 -0700

    Merge Code
    [VV] Target directory needs to be changed, else the code does the job

--- R Code/merge_test.R
+++ R Code/merge_test.R
@@ -1,11 +1,20 @@
+#Collecting all files with PromptResponses
+filenames <- list.files("/Users/vaibhavx/Desktop/R_Data/Input_Files", pattern = 'PromptResponses_EMA.csv$', recursive = T)
+
+#Building path for reading data from the 1st csv
+filepath <- paste("/Users/vaibhavx/Desktop/R_Data/Input_Files", filenames[1], sep = "/")
+
 #Reading the csv files 
+data <- read.csv(filepath, header =T)
 
-data1 <- read.csv("/Users/vaibhavx/Desktop/R_Data/Input_Files/PromptResponses_EMA.csv", header = T)
-data2 <- read.csv("/Users/vaibhavx/Desktop/R_Data/Input_Files/PromptResponses_EMA_copy.csv", header = T)
-data3 <- read.csv("/Users/vaibhavx/Desktop/R_Data/Input_File/PromptResponses_EMA copt 2.csv", header = T)
-
-#Binding all the files row-wise
-final2 <- rbind(data1, data2, data3)
+#looping through different files and then binding them row wise
+for( i in 2:length(filenames)) {
+  
+  filepath <- paste("/Users/vaibhavx/Desktop/R_Data/Input_Files", filenamesp[i], sep="/")
+  data2 <- read.csv(filepath, header = T)
+  data <- rbind(data, data1)
+  
+}
 
 #Writing the files in a single CSV File
-write.csv("/Users/vaibhavx/Desktop/R_Data/merge_test_2", row.names = FALSE)
+write.csv(data,"/Users/vaibhavx/Desktop/R_Data/merge_data.csv", row.names = FALSE)
